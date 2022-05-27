c43104a3b3880830f67ea14b50cfe237486fb6cc
Stefan <s.pavlovic.rs@gmail.com>
2022-05-27 18:15:10 +0200

    Revert "Add everything"
    This reverts commit d697d7cc48da1dc71484952e0a5a0c9e7c1d03c6.

--- README.md
+++
@@ -1,3 +0,0 @@
-# dynamic-ui-practice
-
-For The Odin Project: https://www.theodinproject.com/lessons/node-path-javascript-dynamic-user-interface-interactions#drop-down-menus
--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,1 @@
-/* eslint-disable comma-dangle */
 import '../css/styles.css';
-import carousel from './carousel.change-slides';
-import dropDownMenu from './navigation.drop-down-menu';
-import moreTab from './navigation.more-tab';
-
-dropDownMenu();
-
-moreTab();
-
-carousel();
